fbe172990be41277187480c73511fcc082535353
SG1515 <sunggu507@naver.com>
2022-08-10 11:42:27 +0900

    @GetMapping, @PostMapping, @RequestParam 사용하여 page 시험

--- src/main/java/com/ll/exam/sbb/MainController.java
+++ src/main/java/com/ll/exam/sbb/MainController.java
@@ -1,8 +1,7 @@
 package com.ll.exam.sbb;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class MainController {
@@ -15,6 +14,35 @@ public class MainController {
         //서버에서 실행(console)
         System.out.println("Hello");
         // 먼 미래에 브라우저에서 보여진다.
-        return "안녕하세요.";
+        return "배고파3";
+    }
+
+    @GetMapping("/page1")
+    @ResponseBody
+    public String showPage1() {
+        return """
+                <form method="POST" action="/page2">
+                    <input type="number" name="age" placeholder="나이" />
+                    <input type="submit" value="page2 POST 방식으로 이동" />
+                </form> 
+                """;
+    }
+
+    @PostMapping("/page2")
+    @ResponseBody
+    public String showPage2Post(@RequestParam(defaultValue = "0") int age) {
+        return """
+                <h1>입력된 나이 : %d</h1>
+                <h2>안녕하세요. POST 방식으로 오셨군요.</h2>            
+                """.formatted(age);
+    }
+
+    @GetMapping("/page2")
+    @ResponseBody
+    public String showPage2Get(@RequestParam(defaultValue = "0") int age) {
+        return """
+                <h1>입력된 나이 : %d</h1>
+                <h1>안녕하세요, POST 방식으로 오셨군요.</h1>
+                """.formatted(age);
     }
 }
