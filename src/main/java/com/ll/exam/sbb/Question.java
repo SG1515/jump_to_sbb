118e493f5ad00e4a10f346c4eb434621b19c9d80
SG1515 <sunggu507@naver.com>
2022-08-12 09:40:04 +0900

    Answer 엔티티 추가

--- src/main/java/com/ll/exam/sbb/Question.java
+++ src/main/java/com/ll/exam/sbb/Question.java
@@ -5,6 +5,7 @@ import lombok.Setter;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.List;
 
 
 @Getter
@@ -20,4 +21,6 @@ public class Question {
     @Column(columnDefinition = "TEXT")
     private String content;
     private LocalDateTime createDate;
+    @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
+    private List<Answer> answerList;
 }
