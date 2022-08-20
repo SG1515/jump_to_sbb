1756cbdc010149375a65e2e34f825e62a42c77d0
SG1515 <sunggu507@naver.com>
2022-08-20 14:12:31 +0900

    이미 가입된 회원이라는 이유로 실패할 때, 실패원인을 조금 더 명확히 설명하기, Exception 추가

--- src/main/java/com/ll/exam/sbb/user/UserRepository.java
+++ src/main/java/com/ll/exam/sbb/user/UserRepository.java
@@ -2,4 +2,6 @@ package com.ll.exam.sbb.user;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<SiteUser, Long> {
+    boolean existsByUsername(String username);
+
 }
